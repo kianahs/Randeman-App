d41bb9c26b26d91a1eafcdfb5de93d7324886680
kianahs <hadysadegh@gmail.com>
2021-11-11 20:27:00 +0330

    resource stored procedures created

--- app/src/main/java/com/example/atry/connectToDB.kt
+++ app/src/main/java/com/example/atry/connectToDB.kt
@@ -1,49 +1,66 @@
 package com.example.atry
 
 import com.mysql.cj.jdbc.CallableStatement
-import java.sql.DriverManager
-import java.sql.SQLException
-import java.sql.Types
+import java.sql.*
 import java.util.*
 
-fun main() {
+fun connectToDataBase(): Connection{
     val connectionProps = Properties()
     connectionProps.put("user", "root")
     connectionProps.put("password", "")
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
-        var conn = DriverManager.getConnection(
-            "jdbc:" + "mysql" + "://" +
-                    "127.0.0.1" +
-                    ":" + "3306" + "/" +
-                    "resource_manager_db",
-            connectionProps
-        )
+    Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
 
-        val callableStatement: CallableStatement = conn.prepareCall("{CALL ADD_NEW_RESOURCE(?,?)}") as CallableStatement
+    var conn = DriverManager.getConnection(
+        "jdbc:" + "mysql" + "://" +
+                "127.0.0.1" +
+                ":" + "3306" + "/" +
+                "resource_manager_db",
+        connectionProps
+    )
 
-        callableStatement.setString(1, "JOHNFORD")
-        callableStatement.setString(2, "Nothing")
-        callableStatement.executeQuery()
+    return conn
 
-
-
-
-
-    } catch (ex: SQLException) {
-        // handle any errors
-        ex.printStackTrace()
-    } catch (ex: Exception) {
-        // handle any errors
-        ex.printStackTrace()
-    }
 }
 
-class Resource (val resource_name: String, val resource_ID: Int){
+fun add_new_resource(conn:Connection, name: String, description: String){
+
+    val new_resource = Resource(name, description)
+
+    val callableStatement: CallableStatement = conn.prepareCall("{CALL ADD_NEW_RESOURCE(?,?)}") as CallableStatement
+
+    callableStatement.setString(1, name)
+    callableStatement.setString(2, description)
+    callableStatement.executeQuery()
+
+}
+
+
+fun get_all_resources(conn: Connection): MutableList<Resource>{
+
+    val allResources: MutableList<Resource> = ArrayList()
+
+    val cs: CallableStatement = conn.prepareCall("{CALL SHOW_ALL_RESOURCES()}") as CallableStatement
+    val resultSet: ResultSet = cs.executeQuery()
+
+
+    while (resultSet.next()) {
+
+        val new_resource = Resource(resultSet.getString(2),
+            resultSet.getString(3))
+
+        allResources.add(new_resource)
+
+    }
+
+    return allResources
+
+}
+
+class Resource (val resource_name: String, val resource_description: String){
 
     val tasks_list: MutableList<Task> = ArrayList()
     val name = resource_name
-    val ID = resource_ID
+    val description = resource_description
 
     fun assign_task_to_resource(task: Task){
 
@@ -61,7 +78,9 @@ class Resource (val resource_name: String, val resource_ID: Int){
 
     }
 
-
+    override fun toString(): String {
+        return "Resource(name='$name', description='$description')"
+    }
 
 
 }
@@ -89,17 +108,22 @@ class Task (val task_name: String, val task_ID: Int, val task_duration: Int, val
 
 }
 
-fun start(){
+fun main(){
 
-    val a1 = Task("a1", 1, 20,100)
-    val a2 = Task("a2", 2, 20,700)
-    val a3 = Task("a3", 3, 20,500)
 
-    val CNC = Resource("CNC", 1)
-    CNC.assign_task_to_resource(a2)
-    CNC.assign_task_to_resource(a1)
-    CNC.assign_task_to_resource(a3)
-
-    CNC.sort_tasks_by_priorities()
+    print(get_all_resources(connectToDataBase()))
+//
+//
+//
+//    val a1 = Task("a1", 1, 20,100)
+//    val a2 = Task("a2", 2, 20,700)
+//    val a3 = Task("a3", 3, 20,500)
+//
+//    val CNC = Resource("CNC", )
+//    CNC.assign_task_to_resource(a2)
+//    CNC.assign_task_to_resource(a1)
+//    CNC.assign_task_to_resource(a3)
+//
+//    CNC.sort_tasks_by_priorities()
 
 }
