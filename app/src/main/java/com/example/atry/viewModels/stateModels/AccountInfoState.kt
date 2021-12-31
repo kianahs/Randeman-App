5b9f6ce0a673dc7b59877efe468733f7883df5eb
kianahs <hadysadegh@gmail.com>
2022-01-01 02:46:46 +0330

    Account Info gets from api

--- app/src/main/java/com/example/atry/viewModels/stateModels/AccountInfoState.kt
+++ app/src/main/java/com/example/atry/viewModels/stateModels/AccountInfoState.kt
@@ -6,6 +6,6 @@ import com.example.atry.data.remote.dto.accountInfo
 
 data class AccountInfoState(
     val isLoading:Boolean = false,
-    val informations:accountInfo? = null,
+    val informations:accountInfo? = accountInfo(0,"example@example.com","Not specified","Not specified"),
     val error: String = ""
 )
